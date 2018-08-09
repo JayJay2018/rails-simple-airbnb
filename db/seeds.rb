d8144b63bb5bb195c8fe751ae2126f0f2f0c8b7f
Jascha Rafael Franke <hola@11once.de>
2018-08-09 08:44:06 +0200

    added grid and overview

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Epic place in the middle of nowhere Sachsen',
+  address: 'Tief im Osten 17, 01331 Schwarz',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 25,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Spacious Penthouse in NYC',
+  address: '10 Upper Street NYC W9 1DT',
+  description: 'A lovely Two double bedroom penthouse, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 300,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'shit place in Berlin',
+  address: '102 Samariterstraße Friedrichhain',
+  description: 'One double bedroom, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Tree house in Bangkok',
+  address: 'padda da rein 17 W123 1DT',
+  description: 'large kitchen and a beautiful conservatory, cheap and shitty!',
+  price_per_night: 15,
+  number_of_guests: 1
+)
